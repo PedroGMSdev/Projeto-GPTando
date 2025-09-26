c87802d34686a2864a543ab4fc70e0232f6aaa27
Pedro Guilherme Martins <pedroguilherme_ms@yahoo.com.br>
2025-09-25 22:26:42 -0300

    Criado JavaScript

---
+++ FASE 1 - Variáveis e Tipos de Dados/Desafio 2/calcularSalario.js
@@ -0,0 +1,5 @@
+let salarioMensal = prompt("Digite seu salário mensal: ")
+let mesesTrabalhados = prompt("Digite quantidade de meses trabalhado: ")
+let salarioTotal = salarioMensal * mesesTrabalhados
+
+console.log ("Seu salário total, nesses " , mesesTrabalhados, " meses, é de " , salarioTotal);
