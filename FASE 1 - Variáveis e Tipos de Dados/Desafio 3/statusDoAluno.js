ec952b64f3226af501bfeedfd554fbc2036c4c33
Pedro Guilherme Martins <pedroguilherme_ms@yahoo.com.br>
2025-10-03 19:14:17 -0300

    Desafio 3 JAVASCRIPT

---
+++ FASE 1 - Variáveis e Tipos de Dados/Desafio 3/statusDoAluno.js
@@ -0,0 +1,11 @@
+
+//GPTando - Mini-Missões - Fase 1 - Missão 3
+
+let notaFinal = prompt ("Digite sua nota final: ")
+
+if (notaFinal >= 6) {
+  console.log("Aprovado")
+}
+else {
+  console.log ("Reprovado")
+}
