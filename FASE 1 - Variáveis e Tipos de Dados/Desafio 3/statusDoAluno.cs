98124c9c7a4bba0bde61e82dc1f8f0906f930472
Pedro Guilherme Martins <pedroguilherme_ms@yahoo.com.br>
2025-10-05 12:05:49 -0300

    Desafio 3 C# / Desafio 3 CONCLUÍDO

---
+++ FASE 1 - Variáveis e Tipos de Dados/Desafio 3/statusDoAluno.cs
@@ -0,0 +1,15 @@
+
+//GPTando - Mini-Missões - Fase 1 - Missão 3
+
+using System;
+
+class StatusDoAluno
+{
+    static void Main
+    {
+        Console.Whrite ("Digite sua nota final: ");
+        string notaFinal = double.Parse(Console.ReadLine());
+
+        if ()
+    }
+}
